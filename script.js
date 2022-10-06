55b0c927592812e49e0c0a008bbd0c98d816d50e
Dian Sherman <slayer_barrett_@hotmail.com>
2022-10-06 08:54:11 -0700

    time

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 //   TIME OF DAY
+var timeCal = $("time")
 
+for (var i = 0; i < timeCal.length; i++) {
+    var setTime = 
+    setTime = timeCal[i] + (0900)+1
+    timeCal.setHours(setTime)
+}
+
+console.log(timeCal)
 function displayTime () {
     let today = moment().format("MMM Do, YYYY")
     $("#currentDay").text(today)
@@ -11,37 +19,37 @@ function displayTime () {
 
  
 //   COLOR FOR PASSING OF DAY
-//   function colorBlock () {
-//     let currentTime = moment().hours()
-//     console.log(currentTime)
-//     while (currentTime > timeCLass) {
-//         $('#${timeClass}').addClass("past")
-//     }
-//   }
+  function colorBlock () {
+    let currentTime = moment().hours()
+    console.log(currentTime)
+    while (currentTime > timeCal) {
+        $('#${timeCal}').addClass("past")
+    }
+  }
 
-//   colorBlock()
+  colorBlock()
 
 //   save to local storage
-var textInput = document.querySelector(".textInput");
-var textOutput = document.querySelector(".textOutput")
-var save = document.querySelector("#save")
-        var textArray = JSON.parse(localStorage.getItem("textArray"));
+// var textInput = document.querySelector(".textInput");
+// var textOutput = document.querySelector(".textOutput")
+// var save = document.querySelector("#save")
+//         var textArray = JSON.parse(localStorage.getItem("textArray"));
 
     // var save = )
-save.addEventListener("click", updateOutput) 
-      textOutput.textContent = localStorage.getItem('content')
-      textInput.value = localStorage.getItem('content')
+// save.addEventListener("click", updateOutput) 
+//       textOutput.textContent = localStorage.getItem('content')
+//       textInput.value = localStorage.getItem('content')
 
-function updateOutput() {
-    localStorage.setItem('content', textInput.value);
+// function updateOutput() {
+//     localStorage.setItem('content', textInput.value);
 
-    textOutput.textContent = textInput.value;
-}
-        if (textArray === null) {
-            textArray = [];
-        }
+//     textOutput.textContent = textInput.value;
+// }
+//         if (textArray === null) {
+//             textArray = [];
+//         }
 
-console.log(textInput) 
+// console.log(textInput) 
 
 // localStorage.setItem("activity", JSON.stringify(textInput));
 
